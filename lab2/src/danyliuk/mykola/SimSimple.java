fc07af359b6f5c90e5c7a7043d0c00c07cb17381
mykola <rodrigez6@ukr.net>
2019-11-19 14:06:52 +0200

    Lab2, initial commit

---
+++ lab2/src/danyliuk/mykola/SimSimple.java
@@ -0,0 +1,13 @@
+package danyliuk.mykola;
+
+/**
+ * @author Mykola Danyliuk
+ */
+public class SimSimple {
+
+    public static void main(String[] args) {
+        Model model = new Model(2,1,5);
+        model.simulate(1000);
+    }
+}
+
